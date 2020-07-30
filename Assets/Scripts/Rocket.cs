794d91eeed9d13970825cfd8361891e7b6c811c7
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-07-30 21:59:50 +0200

    Music Add. Bug with mass of rocket fixed by not cliking on Hierarchy before start game.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Thrust();
+        TurnRocket();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void TurnRocket()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
     }
 }
