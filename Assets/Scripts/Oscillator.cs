4c6d0045be11c93d17b9ae18a6b929f384edaa7e
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-08-11 03:20:28 +0200

    add Lights, Oscilator

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
     //todo remove from inspector later
     [Range(0,1)][SerializeField] float movementFactor; //0 for not moved, 1 for fully moved.
@@ -20,6 +21,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //todo protect against period is zero
+        if (period <= Mathf.Epsilon) { return; } 
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1 
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
